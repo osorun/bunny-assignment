6696067ca18bf9a1064b37d1db3387418cfae1b6
Osorun Ifeoluwa <osorunifeoluwa@gmail.com>
2026-04-24 20:52:27 +0100

    Add files via upload

---
+++ bunny.js
@@ -0,0 +1,10 @@
+let bunny = {
+    name: "Fluffy",
+    age: 3,
+    isHappy: true
+};
+
+console.log(bunny);
+console.log("Name:", bunny.name);
+console.log("Age:", bunny.age);
+console.log("Happy?", bunny.isHappy);
