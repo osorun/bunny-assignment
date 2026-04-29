b755080eafa7c82ed0e2e0b8b6c7994a0cd63e29
Osorun Ifeoluwa <osorunifeoluwa@gmail.com>
2026-04-29 19:34:20 +0100

    Update array-exercise.js

--- array-exercise.js
+++ array-exercise.js
@@ -1,4 +1,4 @@
-let bunnies = ["Fluffy", "Bugs-Bunny", "Lola", "Thunder", "Snowball", "Bella"];
+let bunnies = ["Fluffy", "Bugs-Bunny", "Lucy", "Thunder", "Snowball", "Bella"];
 
 bunnies.push("Mario");        // Add to end
 bunnies.splice(2, 1);         // Remove Lucy (at index 2)
