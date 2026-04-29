61ca5b4b93ebc4331d4640010ef114e56ec6b398
Osorun Ifeoluwa <osorunifeoluwa@gmail.com>
2026-04-29 19:21:45 +0100

    array exercise - bunny names with Mario and Luigi

---
+++ array-exercise.js
@@ -0,0 +1,7 @@
+let bunnies = ["Fluffy", "Bugs-Bunny", "Lola", "Thunder", "Snowball", "Bella"];
+
+bunnies.push("Mario");        // Add to end
+bunnies.splice(2, 1);         // Remove Lucy (at index 2)
+bunnies.unshift("Luigi");     // Add to beginning
+
+console.log(bunnies);
